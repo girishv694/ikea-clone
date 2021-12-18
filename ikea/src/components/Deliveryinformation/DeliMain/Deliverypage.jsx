b66036d1dce42a451b73c142c11ab95804b4cf2a
Sumit Singh <sumit47919@gmail.com>
2021-12-18 18:14:26 +0530

    Done del page and 2nd navbar

---
+++ ikea/src/components/Deliveryinformation/DeliMain/Deliverypage.jsx
@@ -0,0 +1,16 @@
+
+
+import {Navbar2} from '../DeliveryCompo/Navbar2/Navbar2';
+import {Status} from '../DeliveryCompo/Status/Status';
+
+import {Deliveryinformation} from '../DeliveryCompo/Deliveryinformation/Deliveryinformation'
+export const Deliverypage =() => {
+
+    return(
+        <>
+        <Navbar2 />
+        <Status />
+        <Deliveryinformation/>
+        </>
+    )
+}
